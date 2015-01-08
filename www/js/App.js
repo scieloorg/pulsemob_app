7848d97bb8298417e5476c27cc2049b80194b6c6
Marcellus Selga Batista <marcellus.batista@infobase.com.br>
2015-01-08 19:06:44 -0200

    Initial commit

---
+++ www/js/App.js
@@ -0,0 +1,242 @@
+(function(window) {
+    // Application object
+    var App = window.App = {
+        isInitialized: false,
+        history: null,
+        scrollApp: null,
+        scrollMenu: null,
+        currentController:null,
+        locale: "pt",
+        $contentLoad:null,
+        $menu: null,
+        $content: null,
+        $contentWrapper: null,
+        $headerApp: null,
+        $headerTitle: null,
+        $loadingDiv: null,
+        $blockDiv: null,
+        $page: null,
+        currentUser: null,
+        constants:{
+            APP_VERSION: "1.0.0"
+        }
+    };
+    
+    //init project
+    App.init = function() {
+        if(!App.isInitialized){
+            
+            App.history = new Array();
+
+            App.setDomElements();
+            App.addEventListeners();
+            App.definitions();
+            App.isInitialized = true;
+                        
+            $.ajaxSetup({
+                statusCode: {
+                    500: function(){
+                        App.showCommonDialog("App", "Ocorreu algum erro na aplicação. Por favor tente novamente mais tarde.");
+                    }
+                }
+            });
+            
+            Navigator.loadPage('home.html');
+        }
+    };
+    
+    App.showMsgErroInternet = function(callback){
+        App.hideLoadingScreen();
+        Navigator.currentModal = new BootstrapDialog({
+            message: 'Confira sua conexão com a internet e clique em OK para tentar novamente.',
+            buttons: [{
+                    label: 'OK',
+                    cssClass: 'btn-default btn-ok',
+                    action: function(dialog) {
+                        dialog.close();
+                        Navigator.currentModal = null;
+                        callback();
+                    }
+                }]
+        });
+        Navigator.currentModal.realize();
+        Navigator.currentModal.open();
+        
+    };
+    
+    //set Application elements
+    App.setDomElements = function() {
+        App.$contentLoad = $("#page-scroller");
+        App.$menu = $("#menu");
+        App.$content = $("#content");
+        App.$headerApp = $('#app-bar');
+        App.$headerTitle = $('#app-bar-title');
+        App.$loadingDiv = $('#loading');
+        App.$blockDiv = $('#block-content');
+        App.$contentWrapper = $("#page-wrapper");
+        App.$page = $("#page");
+    };
+    
+    //set definitions project
+    App.definitions = function() {
+        //fastclick, performance library of mouse events to touch events
+        FastClick.attach(document.body);
+        //block drag "navegator box"
+        $(document).on('touchmove', function(event) {
+            event.preventDefault();
+        });
+    };
+    
+    //set Application listeners
+    App.addEventListeners = function() {
+    	//load internal pages
+        App.$headerApp.on('tap', "#app-bar-menu", Transition.toggleMenu);
+        $("#page").on('tap', '.botoes-app', Navigator.loadPage);
+        
+        document.addEventListener("backbutton", Navigator.backEvent, true);
+        
+        //listener end transition
+        Transition.addEventListeners();
+        //listener menu button
+        
+        //listener swipe events
+        Hammer(document).on("swipeleft", Transition.swipeleftMenu);
+        Hammer(document).on("swiperight", Transition.swiperightMenu);
+        
+        
+        //scroll
+        App.$contentWrapper.height("100%");
+        
+        App.scrollMenu = new IScroll('#menu-content', {scrollbars: false, click: false});
+        App.scrollApp = new IScroll('#page-wrapper', {scrollbars: false, click: false}); 
+        
+        // nao bugar o scroll quando tiver uma tela com input
+        App.scrollApp.on('beforeScrollStart',function() {
+            var focusObj = $(":focus");
+            focusObj.blur();
+        });
+        
+    };
+    
+    App.refreshScroll = function(goTop){
+        setTimeout(function () {
+            App.scrollApp.refresh();
+            if(typeof goTop === 'undefined' || goTop)
+                App.scrollApp.scrollTo(0,0);
+        }, 500);
+    };
+    
+    App.showLoadingScreen = function(){
+        //https://github.com/mobimentum/phonegap-plugin-loading-spinner
+//        window.spinnerplugin.show();
+    };
+    
+    App.hideLoadingScreen = function(){
+        //https://github.com/mobimentum/phonegap-plugin-loading-spinner
+//        window.spinnerplugin.hide();
+    };
+    
+    App.showBackButton = function(){
+        $("#app-bar-title-adjust").hide();
+        $("#app-bar-back").show();
+    };
+    
+    App.hideBackButton = function(){
+        $("#app-bar-back").hide();
+        $("#app-bar-title-adjust").show();
+    };
+    
+    App.showFullPage = function(){
+        App.$page.css('top', '0px');
+        App.$contentWrapper.height("100%");
+        App.$headerApp.fadeOut(400);
+    };
+    
+    App.showNormalPage = function(){
+        App.$page.css('top', App.$headerApp.height());
+        App.$contentWrapper.height(window.innerHeight - App.$headerApp.height());
+        App.$headerApp.fadeIn(1000);
+    };
+    
+    App.showCommonDialog = function(title, msg, cb){
+        App.hideLoadingScreen();
+        Navigator.currentModal = new BootstrapDialog ({
+            message: msg,
+			title: title,
+            buttons: [{
+                label: 'OK',
+                cssClass: 'btn-default btn-ok',
+                action: function(dialog) {
+                    dialog.close();
+                    Navigator.currentModal = null;
+                    if(cb) cb();
+                }
+            }]
+        });
+        
+        Navigator.currentModal.realize();
+        Navigator.currentModal.open();
+    };
+    
+    
+    App.showCustomCommonDialog = function(title, btnLabel, msg, cb){
+        App.hideLoadingScreen();
+        Navigator.currentModal = new BootstrapDialog ({
+            message: msg,
+			title: title,
+            buttons: [{
+                label: btnLabel,
+                cssClass: 'btn-default btn-ok',
+                action: function(dialog) {
+                    dialog.close();
+                    Navigator.currentModal = null;
+                    if(cb) cb();
+                }
+            }]
+        });
+        
+        Navigator.currentModal.realize();
+        Navigator.currentModal.open();
+    };
+    
+    App.showCommonQuestionDialog = function(title, msg, btn1, cb1, btn2, cb2){
+        App.hideLoadingScreen();
+        Navigator.currentModal = new BootstrapDialog ({
+            message: msg,
+            title: title,
+            buttons: [{
+                label: btn1,
+                cssClass: 'btn-default btn-ok',
+                action: function(dialog) {
+                    dialog.close();
+                    Navigator.currentModal = null;
+                    if(cb1) cb1();
+                }
+            },{
+                label: btn2,
+                cssClass: 'btn-default btn-ok',
+                action: function(dialog) {
+                    dialog.close();
+                    Navigator.currentModal = null;
+                    if(cb2) cb2();
+                }
+            }]
+        });
+        
+        Navigator.currentModal.realize();
+        Navigator.currentModal.open();
+    };
+    
+    App.openLink = function(url){
+        if(typeof device !== 'undefined'){
+            if(device.platform === "iOS"){
+                window.open(url, '_system');
+            }else if (device.platform === "Android"){
+                navigator.app.loadUrl(url, { openExternal:true });
+            }
+        }else{
+            window.open(url);
+        }
+    };
+    
+})(window);
---
+++ www/js/Navigator.js
@@ -0,0 +1,105 @@
+(function(window) {
+    //navigator object
+    var Navigator = window.Navigator = {
+        control: true,
+        currentPage: '',
+        currentPageScreenData: null,
+        currentModal:null,
+        fullpage: true
+        
+    };
+    //load page
+    Navigator.backEvent = function(){
+        if(Navigator.currentModal !== null){
+            Navigator.currentModal.close();
+            Navigator.currentModal = null;
+            return;
+        }
+        
+        if(App.$content.hasClass('transitionContentAppStart')){
+            Transition.hideMenu();
+            return;
+        }
+        
+        var state = App.history.pop();
+        if(state){
+            if(state.page){
+                Navigator.currentPage = state.page;
+                Navigator.currentPageScreenData = state.screenData;
+                Transition.control = true;
+                Transition.class = Transition.getClassAnimation(Navigator.currentPage);
+                Transition.start();
+                App.hideLoadingScreen();
+            }
+        }else{ // sai do app
+            Navigator.currentModal = new BootstrapDialog({
+                message: 'Tem certeza que deseja sair do aplicativo?',
+                buttons: [{
+                        label: 'Sim',
+                        action: function(dialog) {
+                            dialog.close();
+                            navigator.app.exitApp();
+                        }
+                    },{
+                        label: 'Não',
+                        action: function(dialog) {
+                            dialog.close();
+                        }
+                    }]
+            });
+            Navigator.currentModal.realize();
+            Navigator.currentModal.open();
+        }
+    };
+    
+    Navigator.loadPage = function(url) {
+        var newPage = null;
+
+        if(typeof url === "string") {
+            newPage = url;
+        }
+        else {
+             newPage = $(this).data("url");
+        }
+        
+        if(Navigator.currentPage === newPage) {
+            Transition.hideMenu();
+            return;
+        }
+        
+        if(!Navigator.fullpage){
+            if(App.currentController && App.currentController.getScreenData){
+                var data = App.currentController.getScreenData();
+                App.history.push({ page: Navigator.currentPage, screenData: data });
+            }else{
+                App.history.push({ page: Navigator.currentPage, screenData: null  });
+            }
+        }else{
+            App.showNormalPage();
+            Navigator.fullpage = false;
+        }
+        
+        Navigator.load(newPage);
+    };
+    
+    
+    Navigator.loadFullPage = function(url) {
+        Navigator.fullpage = true;
+        App.showFullPage();
+        
+        Navigator.load(url);
+    };
+    
+    Navigator.load = function(url){
+        Transition.hideMenu();
+        Transition.control = true;
+        Navigator.currentPage = url;
+        Navigator.currentPageScreenData = null;
+        
+        
+        Transition.class = Transition.getClassAnimation(Navigator.currentPage);
+        return Transition.start();
+    };
+
+
+})(window);
